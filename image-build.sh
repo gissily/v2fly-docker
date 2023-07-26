7b805b872f36919d1133fe987200e5e87364f959
gissily <gissily@outlook.com>
2023-07-26 16:15:57 +0800

    fix build script

--- image-build.sh
+++ image-build.sh
@@ -5,11 +5,12 @@ set -e
 echo " "
 echo " "
 
-LATEST=$(curl https://api.github.com/repos/v2fly/v2ray-core/releases/latest | grep tag_name | cut -d '"' -f 4)
+LATEST=$(curl -s https://api.github.com/repos/v2fly/v2ray-core/releases/latest | jq | grep tag_name | cut -d '"' -f 4)
 
 docker buildx build \
     --platform linux/amd64 \
     --build-arg TAG=${LATEST} \
+    --push \
     -t ${CI_REGISTRY}/gissily/v2fly-docker:${LATEST} \
     -t ${CI_REGISTRY}/gissily/v2fly-docker:latest \
     -f ${PROJECT_DIR}/Dockerfile . --no-cache
