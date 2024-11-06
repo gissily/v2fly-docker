36e87b43bf63a4542e648784e0fdfeb8650aaa94
gissily <gissily@outlook.com>
2024-11-06 20:49:57 +0800

    fix platform

--- image-build.sh
+++ image-build.sh
@@ -8,7 +8,7 @@ echo " "
 LATEST=$(curl -s https://api.github.com/repos/v2fly/v2ray-core/releases/latest | jq | grep tag_name | cut -d '"' -f 4)
 
 docker buildx build \
-    --platform linux/amd64 \
+    --platform linux/amd64,linux/arm64 \
     --build-arg TAG=${LATEST} \
     --push \
     -t ${CI_REGISTRY}/gissily/v2fly-core:${LATEST} \
